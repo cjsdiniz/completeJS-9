9f9372aab219affaf58fa4646db92660b37a664e
cjsdiniz <cjsdiniz@outlook.com>
2022-04-21 15:31:10 +0100

    Update modernops.js

--- modernops.js
+++ modernops.js
@@ -26,14 +26,20 @@ const restaurant = {
 
   // ES6 enhanced object literals
   openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
 
+// #Destructuring
+// ## Stardard
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
-//Destructuring
+// ## Simple destructuring
 const [x, y, z] = arr;
 console.log(x, y, z);
 console.log(arr);
@@ -41,6 +47,36 @@ console.log(arr);
 const [fst, , snd] = restaurant.categories;
 console.log(fst, snd);
 
-// Switch Arrays
+// ## Switch Arrays
 let [s, f] = [fst, snd];
 console.log(f, s);
+
+// ## Destruct examples
+// ### 1.
+console.log('🚩', restaurant.order(2, 0));
+const [stt, main] = restaurant.order(2, 0);
+console.log(stt, main);
+
+// ### 2.
+const nest = [2, 4, [6, 8]];
+
+// ## Simple Destruct
+const [i, , j] = nest;
+console.log(i, j);
+
+// ## Nested Destruct
+const [k, , [l, m]] = nest;
+console.log(k, l, m);
+
+// ## Destructuring using default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+
+// ###
+const arr1 = [7, 8, 9];
+const bnArr = [1, 2, arr1];
+console.log(bnArr);
+
+const nArr = [1, 2, ...arr1];
+console.log(nArr);
+console.log(...nArr);
