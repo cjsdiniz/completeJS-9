8606967504f5bcddb533eeb3281b87291ead833a
cjsdiniz <cjsdiniz@outlook.com>
2022-04-21 10:34:18 +0100

    Update modernops.js

--- modernops.js
+++ modernops.js
@@ -27,3 +27,20 @@ const restaurant = {
   // ES6 enhanced object literals
   openingHours,
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//Destructuring
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+const [fst, , snd] = restaurant.categories;
+console.log(fst, snd);
+
+// Switch Arrays
+let [s, f] = [fst, snd];
+console.log(f, s);
