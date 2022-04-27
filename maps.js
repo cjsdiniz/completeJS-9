3cd33167c33f5780128d2ac5108ade76fab16cd4
cjsdiniz <cjsdiniz@outlook.com>
2022-04-27 08:40:00 +0100

    Update maps.js

--- maps.js
+++ maps.js
@@ -63,5 +63,16 @@ rest.set('name', 'Italiano');
 rest.set(1, 'Firenze, Italy');
 rest.set(2, 'Lisbon, Portugal');
 
-rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']);
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
 console.log(rest);
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
