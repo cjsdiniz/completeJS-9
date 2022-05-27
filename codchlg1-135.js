36ada810d143535c611849a822bff3e817ae3116
cjsdiniz <cjsdiniz@outlook.com>
2022-05-27 18:04:01 +0100

    Update codchlg1-135.js

--- codchlg1-135.js
+++ codchlg1-135.js
@@ -4,27 +4,31 @@ const poll = {
   question: 'What is your favorite programming language?',
   options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
   answers: new Array(4).fill(0),
-};
-
-const registerNewAnswer = {
-  act: () => {
-    poll.answers;
-    console.log(poll.question);
-    for (let i = 0; i < poll.options.length; i++) {
-      console.log(poll.options[i]);
-    }
-    console.log('(Write option number)');
-    let n = 4;
-    while (n > 3 || n < 0) {
-      n = Number(prompt('Please enter a number (0 to 3)'));
-    }
-
-    registerNewAnswer.addAnswer(n);
-    registerNewAnswer.displayResults('string');
-  },
 
   addAnswer(ans) {
     poll.answers[ans] += 1;
+  },
+
+  registerNewAnswer() {
+    const msg =
+      poll.question +
+      '\n' +
+      poll.options.join('\n') +
+      '\n(Write option number)';
+    // poll.answers;
+    // console.log();
+    // console.log());
+    // for (let i = 0; i < poll.options.length; i++) {
+    //   console.log(poll.options[i]);
+    // }
+    // console.log();
+    let n = 4;
+    while (n > 3 || n < 0) {
+      n = Number(prompt(msg + '\nPlease enter a number (0 to 3)'));
+    }
+
+    this.addAnswer(n);
+    this.displayResults('string');
   },
 
   displayResults(type) {
@@ -33,9 +37,12 @@ const registerNewAnswer = {
     );
   },
 };
-//registerNewAnswer.menu();
 
+// console.log(poll.answers);
+
+// const
+// };
+//registerNewAnswer.menu();
 document
   .querySelector('.poll')
-  .addEventListener('click', registerNewAnswer.act);
-// console.log(poll.answers);
+  .addEventListener('click', poll.registerNewAnswer);
