3e4a9fd1b79b200f44f4e5864ad6f69e64bfa41d
cjsdiniz <cjsdiniz@outlook.com>
2022-06-07 19:19:46 +0100

    DT update

--- datatransform.js
+++ datatransform.js
@@ -1,1 +1,13 @@
 'use strict';
+const arr = [3, 1, 4, 3, 2];
+
+// MAP
+const map = arr.map(x => x * 2);
+console.log('Arr: ', arr);
+console.log('Map: ', map);
+
+// FILTER
+console.log(arr.filter(x => x > 2));
+
+// REDUCE
+console.log(arr.reduce((p, c) => p / c));
