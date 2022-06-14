2b8c158f33a04ea1bbe923850b1bc94e6dd901b1
cjsdiniz <cjsdiniz@outlook.com>
2022-06-14 18:04:18 +0100

    Update codchlg2-154.js

--- codchlg2-154.js
+++ codchlg2-154.js
@@ -6,7 +6,7 @@ const dogs = [...dogsJulia, ...dogsKate];
 
 const calcAverageHumanAge = dogs => {
   const dogsAgeHuman = dogs.map(x => (x <= 2 ? x * 2 : 16 + x * 4));
-  const dogsAgeHuman18plus = dogsAgeHuman.filter(x => x > 18);
+  const dogsAgeHuman18plus = dogsAgeHuman.filter(x => x >= 18);
   console.log(`Dogs ages: ${dogs}`);
   console.log(`1) Dogs ages human like: ${dogsAgeHuman}`);
   console.log(`2) Dogs ages human like > 18: ${dogsAgeHuman18plus}`);
@@ -19,7 +19,7 @@ const calcAverageHumanAge = dogs => {
 //TEST DATA
 const data1 = [5, 2, 4, 1, 15, 8, 3];
 const data2 = [16, 6, 10, 5, 6, 1, 4];
-calcAverageHumanAge(dogs);
+// calcAverageHumanAge(dogs);
 calcAverageHumanAge(data1);
 calcAverageHumanAge(data2);
 //console.log(Math.average(dogs));
